4cabdc11eb1565b115ff65778f5d70abcaabf31c
SuperEVO <zhang740@qq.com>
2015-03-23 09:18:00 +0800

    代码优化，添加功能
    代码优化，添加预渲染，添加repeat，修复多故事板同时操作一个元素效果不叠加的bug

--- animationEngine.action.js
+++ animationEngine.action.js
@@ -1,72 +1,69 @@
 (function() {
     if (window.AE) {
         var engine = window.AE.engine;
-        engine.Addhandler("discrete", function(discrete, getAttr) {
-            var data = {
-                value: getAttr(discrete, 'value')
+        engine.Addhandler("discrete", true, function(time, dom, getAttr) {
+            return {
+                value: getAttr(dom, 'value')
             };
-            return data;
         }, function(time, pre, data, next) {
             return {
                 pass: true,
-                value: data.data.value
+                value: data.value
             };
         });
 
-        engine.Addhandler("easing", function(easing, getAttr) {
-            var value = getAttr(easing, 'value');
-            var data = {
-                value: Number.parseInt(value),
+        engine.Addhandler("easing", false, function(time, dom, getAttr) {
+            return {
+                time: time,
+                value: parseInt(getAttr(dom, 'value'))
             };
-            return data;
         }, function(time, pre, data, next) {
             if (!next) {
                 return {
                     pass: true,
-                    value: data.data.value
+                    value: data.value
                 };
             }
             var result = {
                 pass: next.time < time
             };
             if (next.time < time) {
-                result.value = next.data.value;
+                result.value = next.value;
             } else {
-                result.value = data.data.value + (time - data.time) / (next.time - data.time) * (next.data.value - data.data.value);
+                result.value = data.value + (time - data.time) / (next.time - data.time) * (next.value - data.value);
             }
             return result;
         });
 
-        engine.Addhandler("easingcolor", function(easing, getAttr) {
-            var value = getAttr(easing, 'value');
-            var data = {
+        engine.Addhandler("easingcolor", false, function(time, dom, getAttr) {
+            var value = getAttr(dom, 'value');
+            return {
+                time: time,
                 value: value,
-                r: Number.parseInt(value.substr(0, 2), 16),
-                g: Number.parseInt(value.substr(2, 2), 16),
-                b: Number.parseInt(value.substr(4, 2), 16),
+                r: parseInt(value.substr(0, 2), 16),
+                g: parseInt(value.substr(2, 2), 16),
+                b: parseInt(value.substr(4, 2), 16)
             };
-            return data;
         }, function(time, pre, data, next) {
             if (!next) {
                 return {
                     pass: true,
-                    value: data.data.value
+                    value: data.value
                 };
             }
             var result = {
                 pass: next.time < time
             };
             if (next.time < time) {
-                result.value = next.data.value;
+                result.value = next.value;
             } else {
-                var r = data.data.r + (time - data.time) / (next.time - data.time) * (next.data.r - data.data.r);
-                var g = data.data.g + (time - data.time) / (next.time - data.time) * (next.data.g - data.data.g);
-                var b = data.data.b + (time - data.time) / (next.time - data.time) * (next.data.b - data.data.b);
-                r = ("0" + Number.parseInt(r).toString(16)).slice(-2);
-                g = ("0" + Number.parseInt(g).toString(16)).slice(-2);
-                b = ("0" + Number.parseInt(b).toString(16)).slice(-2);
+                var r = data.r + (time - data.time) / (next.time - data.time) * (next.r - data.r);
+                var g = data.g + (time - data.time) / (next.time - data.time) * (next.g - data.g);
+                var b = data.b + (time - data.time) / (next.time - data.time) * (next.b - data.b);
+                r = ("0" + parseInt(r).toString(16)).slice(-2);
+                g = ("0" + parseInt(g).toString(16)).slice(-2);
+                b = ("0" + parseInt(b).toString(16)).slice(-2);
                 result.value = r + g + b;
-                console.log(result.value);
             }
             return result;
         });
---
+++ animationEngine.design.js
@@ -0,0 +1,117 @@
+(function() {
+    if (window.AE) {
+        var engine = window.AE.engine,
+            document = window.document,
+            Event = {
+                addEvent: function(element, type, handler) {
+                    if (element.addEventListener) {
+                        element.addEventListener(type, handler, false);
+                    } else if (element.attachEvent) {
+                        element.attachEvent('on' + type, function() {
+                            handler.call(element);
+                        });
+                    } else {
+                        element['on' + type] = handler;
+                    }
+                },
+                removeEvent: function(element, type, handler) {
+                    if (element.removeEventListener) {
+                        element.removeEventListener(type, handler, false);
+                    } else if (element.datachEvent) {
+                        element.detachEvent('on' + type, handler);
+                    } else {
+                        element['on' + type] = null;
+                    }
+                },
+                stopPropagation: function(ev) {
+                    if (ev.stopPropagation) {
+                        ev.stopPropagation();
+                    } else {
+                        ev.cancelBubble = true;
+                    }
+                },
+                preventDefault: function(event) {
+                    if (event.preventDefault) {
+                        event.preventDefault();
+                    } else {
+                        event.returnValue = false;
+                    }
+                },
+                getTarget: function(event) {
+                    return event.target || event.srcElement;
+                },
+                getEvent: function(e) {
+                    var ev = e || window.event;
+                    if (!ev) {
+                        var c = this.getEvent.caller;
+                        while (c) {
+                            ev = c.arguments[0];
+                            if (ev && Event == ev.constructor) {
+                                break;
+                            }
+                            c = c.caller;
+                        }
+                    }
+                    return ev;
+                }
+
+            },
+            Objects = {};
+
+        engine.design = {
+            _panel: null,
+            _timePanel: null,
+            _workspace: null,
+            _lastover: null,
+            onmouseover: function(e) {
+                var target = e.target;
+                var ts = target.style;
+                ts.backgroundColor = "rgba(255, 0, 0, 0.25)";
+                ts.boxShadow = "5px 5px 3px #888888";
+                ts.outline = "red solid 1px";
+                ts.cursor = "pointer";
+                engine.design._lastover = target;
+            },
+            onmouseout: function(e) {
+                var target = e.target;
+                var ts = target.style;
+                ts.backgroundColor = "";
+                ts.boxShadow = "";
+                ts.outline = "";
+                ts.cursor = "";
+            },
+            onclick: function(e) {
+                var target = e.target;
+                document.getElementById('designTarget').innerHTML = e.target.tagName;
+                document.getElementById('designTargetId').innerHTML = e.target.id ? e.target.id : '无';
+            },
+            init: function(dom) {
+                if (this._workspace) return;
+                this._panel = document.getElementById('designPanel');
+                this._timePanel = document.getElementById('designTimePanel');
+                if (typeof dom == "string") dom = document.getElementById(dom);
+                Event.addEvent(dom, 'mouseover', this.onmouseover);
+                Event.addEvent(dom, 'mouseout', this.onmouseout);
+                Event.addEvent(dom, 'click', this.onclick);
+                this._workspace = dom;
+
+                this._panel.style.display = 'block';
+                this._timePanel.style.display = 'block';
+            },
+            close: function() {
+                if (!this._workspace) return;
+                Event.removeEvent(this._workspace, 'mouseover', this.onmouseover);
+                Event.removeEvent(this._workspace, 'mouseout', this.onmouseout);
+                Event.removeEvent(this._workspace, 'click', this.onclick);
+                if (this._lastover) {
+                    this.onmouseout({
+                        target: this._lastover
+                    });
+                }
+                this._workspace = null;
+                this._panel.style.display = 'none';
+                this._timePanel.style.display = 'none';
+            }
+        };
+    }
+}());
